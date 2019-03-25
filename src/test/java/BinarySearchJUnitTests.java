d7fde5c53bba16a6785f74aa42c624501ee8826b
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-25 01:53:32 +0100

    testy_v1 testOneElementIsInSequence added - test failed

--- src/test/java/BinarySearchJUnitTests.java
+++ src/test/java/BinarySearchJUnitTests.java
@@ -1,3 +1,19 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BinarySearchJUnitTests {
+
+    @Test public void testOneElementIsInSequence() {
+        int key = 3;
+        int[] seq = {3};
+        boolean testValue = true;
+        SearchResult searchResult;
+        searchResult = BinarySearch.search(key, seq);
+
+        Assert.assertEquals(testValue, searchResult.isFound());
+        Assert.assertEquals(key, seq[searchResult.getPosition()]);
+    }
 
 }
