4fc670b91596efc71f1a07af90cf8d276a1ba9ec
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-25 03:26:55 +0100

    testy_v1 testIsInTheMiddleOfSequence added - test passed

--- src/test/java/BinarySearchJUnitTests.java
+++ src/test/java/BinarySearchJUnitTests.java
@@ -29,7 +29,7 @@ public class BinarySearchJUnitTests {
 
     @Test public void testIsFirstInSequence() {
         int key = 3;
-        int[] seq = {3,4,5};
+        int[] seq = {3, 4, 5};
         boolean testValue = true;
         int testPosition = 0;
         SearchResult searchResult = BinarySearch.search(key, seq);
@@ -40,13 +40,27 @@ public class BinarySearchJUnitTests {
 
     @Test public void testIsLastInSequence() {
         int key = 5;
-        int[] seq = {3,4,5};
+        int[] seq = {3, 4, 5};
         boolean testValue = true;
-        int testPosition = seq.length-1;
+        int testPosition = seq.length - 1;
         SearchResult searchResult = BinarySearch.search(key, seq);
 
         Assert.assertEquals(testValue, searchResult.isFound());
         Assert.assertEquals(testPosition, searchResult.getPosition());
+    }
+
+    @Test public void testIsInTheMiddleOfSequence() {
+        int key = 5;
+        int[] seq = {3, 4, 5, 6, 7};
+        boolean testValue = true;
+        boolean hasOddLength = seq.length % 2 != 0;
+        int middlePosition = (seq.length - 1) / 2;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+
+        Assert.assertEquals(testValue, hasOddLength);
+        Assert.assertEquals(testValue, searchResult.isFound());
+        Assert.assertEquals(middlePosition, searchResult.getPosition());
     }
 
 }
