3378aae9b54951541ed7a5ea8d0c85cf9ecc030e
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-26 22:28:36 +0100

    testy_v1 Testy dla kilku elementów sekwencji wejściowej

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -19,4 +19,32 @@ public class BinarySearchTest {
         SearchResult sr = BinarySearch.search(1, seq);
         Assert.assertFalse(sr.isFound());
     }
+
+    @Test
+    public void shouldReturnTrueIfElementIsFoundInTheBeginning() {
+        int[] seq = {1, 2, 3, 4};
+        SearchResult sr = BinarySearch.search(1, seq);
+        Assert.assertTrue(sr.isFound());
+    }
+
+    @Test
+    public void shouldReturnTrueIfElementIsFoundInTheEnd() {
+        int[] seq = {1, 2, 3, 4};
+        SearchResult sr = BinarySearch.search(4, seq);
+        Assert.assertTrue(sr.isFound());
+    }
+
+    @Test
+    public void shouldReturnTrueIfElementIsFoundInTheMiddle() {
+        int[] seq = {1, 2, 3, 4};
+        SearchResult sr = BinarySearch.search(2, seq);
+        Assert.assertTrue(sr.isFound());
+    }
+
+    @Test
+    public void shouldReturnFalseIfElementIsNotFound() {
+        int[] seq = {1, 2, 3, 4};
+        SearchResult sr = BinarySearch.search(0, seq);
+        Assert.assertFalse(sr.isFound());
+    }
 }
