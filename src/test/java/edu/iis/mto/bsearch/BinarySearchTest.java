ec7dc6ec671d20619327eda593acf3110a6f0709
209450 <209450@edu.p.lodz.pl>
2019-03-27 15:30:49 +0100

    testy_v2 - Zmiana assertów na assertThat

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.bsearch;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class BinarySearchTest {
@@ -16,33 +17,33 @@ public class BinarySearchTest {
 
     @Test public void keyIsInSeq() {
         seq = new int[]{key};
-        assertEquals(true,BinarySearch.search(key,seq).isFound());
+        assertThat(true,is(BinarySearch.search(key,seq).isFound()));
     }
 
     @Test public void keyNotInSeq() {
         seq = new int[]{3};
-        assertEquals(false,BinarySearch.search(key,seq).isFound());
+        assertThat(false,is(BinarySearch.search(key,seq).isFound()));
     }
 
     @Test public void keyIsFirstKeyInSeq() {
         seq = new int[]{key,2,3};
-        assertEquals(1,BinarySearch.search(key,seq).getPosition());
+        assertThat(1,is(BinarySearch.search(key,seq).getPosition()));
     }
 
     @Test public void KeyIsLastKeyInSeq() {
         key = 4;
         seq = new int[]{1,2,3,key};
-        assertEquals(4,BinarySearch.search(key,seq).getPosition());
+        assertThat(4,is(BinarySearch.search(key,seq).getPosition()));
     }
 
     @Test public void KeyIsCenterKeyInSeq() {
         key = 3;
         seq = new int[]{1,2,key,6,7};
-        assertEquals(3,BinarySearch.search(key,seq).getPosition());
+        assertThat(3,is(BinarySearch.search(key,seq).getPosition()));
     }
 
     @Test public void NoKeyInSeq() {
         seq = new int[]{2,3,4,10};
-        assertEquals(false,BinarySearch.search(key,seq).isFound());
+        assertThat(false,is(BinarySearch.search(key,seq).isFound()));
     }
 }
