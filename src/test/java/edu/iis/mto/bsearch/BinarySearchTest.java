1812d4aefb4fd42a9c386cfbe8bd8dcd01b36852
Szymon Zgola <kobold97@gmail.com>
2019-04-10 03:02:20 +0200

    Formater poprawiony, dane zróżnicowane

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,52 +8,70 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test
-	public void onlyOneGoodElementIsInTheSequence() throws Exception {
-		int[] seq = { 1 };
-		assertThat((BinarySearch.search(1, seq).isFound() == true && BinarySearch.search(1, seq).getPosition() == 0),
-				is(true));
-	}
+    @Test
+    public void onlyOneGoodElementIsInTheSequence() throws Exception {
+        int[] seq = {1};
+        assertThat((BinarySearch.search(1, seq)
+                                .isFound() == true
+                    && BinarySearch.search(1, seq)
+                                   .getPosition() == 0),
+                is(true));
+    }
 
-	@Test
-	public void onlyOneBadElementIsInTheSequence() throws Exception {
-		int[] seq = { 1 };
-		assertThat((BinarySearch.search(2, seq).isFound() == false && BinarySearch.search(2, seq).getPosition() == -1),
-				is(true));
-	}
+    @Test
+    public void onlyOneBadElementIsInTheSequence() throws Exception {
+        int[] seq = {1};
+        assertThat((BinarySearch.search(2, seq)
+                                .isFound() == false
+                    && BinarySearch.search(2, seq)
+                                   .getPosition() == -1),
+                is(true));
+    }
 
-	@Test
-	public void multipleElementsSequenceWithGoodElementAtTheBeggining() throws Exception {
-		int[] seq = { 1, 2, 3, 4 };
-		assertThat((BinarySearch.search(1, seq).isFound() == true && BinarySearch.search(1, seq).getPosition() == 0),
-				is(true));
-	}
+    @Test
+    public void multipleElementsSequenceWithGoodElementAtTheBeggining() throws Exception {
+        int[] seq = {1, 2, 3, 4};
+        assertThat((BinarySearch.search(1, seq)
+                                .isFound() == true
+                    && BinarySearch.search(1, seq)
+                                   .getPosition() == 0),
+                is(true));
+    }
 
-	@Test
-	public void multipleElementsSequenceWithGoodElementInTheMiddle() throws Exception {
-		int[] seq = { 1, 2, 3, 4, 5 };
-		assertThat((BinarySearch.search(3, seq).isFound() == true && BinarySearch.search(3, seq).getPosition() == 2),
-				is(true));
-	}
+    @Test
+    public void multipleElementsSequenceWithGoodElementInTheMiddle() throws Exception {
+        int[] seq = {4, 5, 6, 7};
+        assertThat((BinarySearch.search(7, seq)
+                                .isFound() == true
+                    && BinarySearch.search(7, seq)
+                                   .getPosition() == 3),
+                is(true));
+    }
 
-	@Test
-	public void multipleElementsSequenceWithGoodElementAtTheEnd() throws Exception {
-		int[] seq = { 1, 2, 3, 4 };
-		assertThat((BinarySearch.search(4, seq).isFound() == true && BinarySearch.search(4, seq).getPosition() == 3),
-				is(true));
-	}
+    @Test
+    public void multipleElementsSequenceWithGoodElementAtTheEnd() throws Exception {
+        int[] seq = {1, 2, 3, 4};
+        assertThat((BinarySearch.search(4, seq)
+                                .isFound() == true
+                    && BinarySearch.search(4, seq)
+                                   .getPosition() == 3),
+                is(true));
+    }
 
-	@Test
-	public void multipleElementsSequenceWithBadElement() throws Exception {
-		int[] seq = { 1, 2, 3, 4 };
-		assertThat((BinarySearch.search(8, seq).isFound() == false && BinarySearch.search(8, seq).getPosition() == -1),
-				is(true));
-	}
+    @Test
+    public void multipleElementsSequenceWithBadElement() throws Exception {
+        int[] seq = {1, 2, 3, 4};
+        assertThat((BinarySearch.search(8, seq)
+                                .isFound() == false
+                    && BinarySearch.search(8, seq)
+                                   .getPosition() == -1),
+                is(true));
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void throwsIllegalArgumentException() throws Exception {
-		int[] seq = {};
-		BinarySearch.search(2, seq);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsIllegalArgumentException() throws Exception {
+        int[] seq = {};
+        BinarySearch.search(2, seq);
+    }
 
 }
